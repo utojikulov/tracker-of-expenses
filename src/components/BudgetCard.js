94e54da61167306ff1e31e123b24178b6a77f564
dygvonajouza <cookie.pechehbkov@mail.ru>
2024-12-22 10:32:20 +0500

    1st commit

---
+++ src/components/BudgetCard.js
@@ -0,0 +1,42 @@
+import { Card, ProgressBar, Button, Stack } from "react-bootstrap";
+import { currencyFormatter } from "../utils"
+
+export default function BudgetCard({name, amount, max }) {
+  return (
+    <Card>
+      <Card.Body>
+        <Card.Title className="d-flex justify-content-between align-items-baseline mb-3 fw-normal">
+          <div className="me-2">{ name }</div>
+          <div className="d-flex align-items-baseline">
+            { currencyFormatter.format(amount) } /
+            <span className="text-muted fs-6 ms-1">{ currencyFormatter.format(max) }</span>
+            </div>
+        </Card.Title>
+        <ProgressBar
+          className="rounded-pill"
+          variant={getProgressBarVariant(amount, max)}
+          min={0}
+          max={max}
+          now={amount}
+          />
+        <Stack direction="horizontal" gap="2" className="mt-4">
+          <Button variant="outline-primary" className="ms-auto">
+            Add Expense
+            </Button>
+
+          <Button variant="outline-secondary" >
+            View Expenses
+            </Button>
+
+        </Stack>
+      </Card.Body>
+    </Card>
+  )
+
+  function getProgressBarVariant(amount, max) {
+    const ratio = amount / max
+    if (ratio < .5) return "primary"
+    if (ratio < .75) return "warning"
+    return "danger"
+  }
+}
