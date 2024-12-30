e299d91ec055a29e796e2460b214e84d7c5f8b97
dygvonajouza <122049107+dygvonajouza@users.noreply.github.com>
2024-12-30 12:30:36 +0000

    bmc should fix

--- src/App.js
+++ src/App.js
@@ -1,31 +1,38 @@
 import Container from "react-bootstrap/Container"
 import {Button, Stack} from "react-bootstrap"
 import BudgetCard from "./components/BudgetCard.js"
+import AddBudgetModal  from "./components/AddBudgetModal.js"
+import { set } from "date-fns"
+import { useState } from "react"
 
 function App() {
+  const [showAddBudgetModal, setShowAddBudgetModal] = useState(false)
   return (
-    <Container className="my-4">
-      <Stack direction="horizontal" gap="2" className="mb-4">
-        <h1 className="me-auto">Budgets</h1>
-        <Button variant="primary">Add Budget</Button>
-        <Button variant="outline-primary2 1">Add Expense</Button>
-      </Stack>
+    <>
+      <Container className="my-4">
+        <Stack direction="horizontal" gap="2" className="mb-4">
+          <h1 className="me-auto">Budgets</h1>
+          <Button variant="primary">Add Budget</Button>
+          <Button variant="outline-primary2 1">Add Expense</Button>
+        </Stack>
 
-        <div
-          style={{
-           display:"grid",
-            gridTemplateColumns:"repeat(auto-fill, minmax(300px, 1fr))",
-            gap:"1rem",
-            alignItems:"flex-start",
-          }}
-        >
-          <BudgetCard
-            name={"Groceries"}
-            gray
-            amount={200}
-            max={3000}
-          />
-        </div>
-    </Container>
+          <div
+            style={{
+            display:"grid",
+              gridTemplateColumns:"repeat(auto-fill, minmax(300px, 1fr))",
+              gap:"1rem",
+              alignItems:"flex-start",
+            }}
+          >
+            <BudgetCard
+              name={"Groceries"}
+              gray
+              amount={200}
+              max={3000}
+            />
+          </div>
+      </Container>
+      <AddBudgetModal show={showAddBudgetModal} handleClose={() => setShowAddBudgetModal(false)}/>
+    </>
   )
 } export default App
